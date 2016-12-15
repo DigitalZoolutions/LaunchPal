7aeaed2ed9a8a28742dc24d0d7edd261741fc782
Kim Böndergaard <threezool@gmail.com>
2016-12-15 21:50:26 +0100

    Added support for caching tracked agencies and added a model to hold the data

--- LaunchPal/LaunchPal/Model/CacheModel/CacheTracking.cs
+++ LaunchPal/LaunchPal/Model/CacheModel/CacheTracking.cs
@@ -5,5 +5,6 @@ namespace LaunchPal.Model.CacheModel
     class CacheTracking : CacheBase
     {
         public List<LaunchData> TrackingList { get; set; }
+        public List<TrackedAgency> TrackedAgencies { get; set; }
     }
 }
---
+++ LaunchPal/LaunchPal/Model/TrackedAgency.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LaunchPal.Enums;
+
+namespace LaunchPal.Model
+{
+    public class TrackedAgency
+    {
+        public AgencyType AgencyType { get; set; }
+
+        public int ScheduledLaunches => ScheduledLaunchData.Count;
+
+        public List<LaunchData> ScheduledLaunchData { get; set; }
+        public int PlanedLaucnhes => PlanedLaunchData.Count;
+        public List<LaunchData> PlanedLaunchData { get; set; }
+        public DateTime CacheTimeOut { get; set; }
+    }
+}
